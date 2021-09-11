33230b1d24c9b14e8846d3a81bd846bf6d10a9da
BartłomiejRocławski <b.roclawski@gmail.com>
2021-09-11 22:02:01 +0200

    A class added for octal conversion

--- src/DecToOctalConverter.java
+++ src/DecToOctalConverter.java
@@ -8,24 +8,15 @@ public class DecToOctalConverter extends Converter {
 
     public void convert(int quotient) {
         do {
-            switch (quotient % 8) {
-                case 0 -> {
-                    this.bits.add(this.index, 0);
-                    this.index++;
-                }
-                case 1 -> {
-                    this.bits.add(index, 1);
-                    this.index++;
-                }
-                default -> throw new IllegalStateException("Unexpected value: " + quotient % 8);
-            }
-            quotient /= 2;
+            this.bits.add(this.index, quotient % 8);
+            this.index++;
+            quotient /= 8;
         } while (quotient > 0);
 
         Collections.reverse(this.bits);
 
         for (Integer bit : this.bits) {
-            System.out.print(bit + " ");
+            System.out.print(bit);
         }
     }
 }
