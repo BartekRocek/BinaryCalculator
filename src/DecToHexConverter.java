d3b71aa13ce1c02aefb089b59774769dee15cfbf
BartłomiejRocławski <b.roclawski@gmail.com>
2021-09-12 12:04:38 +0200

    slimmed done the code by removing index++ from each case in the switch statement and moving it out of it

--- src/DecToHexConverter.java
+++ src/DecToHexConverter.java
@@ -10,35 +10,15 @@ public class DecToHexConverter extends Converter {
     public void convert(int quotient) {
         do {
             switch (quotient % 16) {
-                case 10 -> {
-                    this.bits.add(this.index, "A");
-                    this.index++;
-                }
-                case 11 -> {
-                    this.bits.add(this.index, "B");
-                    this.index++;
-                }
-                case 12 -> {
-                    this.bits.add(this.index, "C");
-                    this.index++;
-                }
-                case 13 -> {
-                    this.bits.add(this.index, "D");
-                    this.index++;
-                }
-                case 14 -> {
-                    this.bits.add(this.index, "E");
-                    this.index++;
-                }
-                case 15 -> {
-                    this.bits.add(this.index, "F");
-                    this.index++;
-                }
-                default -> {
-                    this.bits.add(this.index, Integer.toString(quotient % 16));
-                    this.index++;
-                }
+                case 10 -> this.bits.add(this.index, "A");
+                case 11 -> this.bits.add(this.index, "B");
+                case 12 -> this.bits.add(this.index, "C");
+                case 13 -> this.bits.add(this.index, "D");
+                case 14 -> this.bits.add(this.index, "E");
+                case 15 -> this.bits.add(this.index, "F");
+                default -> this.bits.add(this.index, Integer.toString(quotient % 16));
             }
+            this.index++;
             quotient /= 16;
         } while (quotient > 0);
 
